e678bb59ca52dd99efc690f29bd9dd321b37d861
ktopchiev <karlostopchiev@gmail.com>
2021-01-13 19:40:11 +0200

    solved

---
+++ C-Sharp-Avanced/Stacks-And-Queues/Reverse-Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+
+            Stack<string> reversedStr = new Stack<string>();
+
+            foreach (var ch in input)
+            {
+                reversedStr.Push(ch.ToString());
+            }
+
+            Console.WriteLine(string.Join("", reversedStr));
+        }
+    }
+}
