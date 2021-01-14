eb58c15240c326e21bab7716830bee88ad2f71d3
ktopchiev <karlostopchiev@gmail.com>
2021-01-14 17:22:44 +0200

    solved print even numbers

---
+++ C-Sharp-Avanced/Stacks-And-Queues/Print-Even-Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Print_Even_Numbers
+{
+    /*
+        Write program that:
+         Reads an array of integers and adds them to a queue
+         Prints the even numbers separated by "," 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            Queue<int> ints = new Queue<int>(integers);
+            
+            while (ints.Count > 0)
+            {
+                int num = ints.Dequeue();
+                if (num % 2 == 0)
+                {
+                    Console.Write(num);
+
+                    if (ints.Count > 0)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+            }
+        }
+    }
+}
