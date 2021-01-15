6605a5769608a5966c0f9f8c1631860d31cbdaa0
ktopchiev <karlostopchiev@gmail.com>
2021-01-15 19:22:24 +0200

    solved songs queue

---
+++ C-Sharp-Avanced/Stacks-And-Queues/Songs-Queue/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine().Split(", ");
+
+            Queue<string> songsQueue = new Queue<string>(songs);
+
+            while (songsQueue.Count > 0)
+            {
+                string[] commandInput = Console.ReadLine().Split();
+
+                string command = commandInput[0];
+
+                if (command == "Play")
+                {
+                    songsQueue.Dequeue();
+                }
+                else if (command == "Add")
+                {
+                    string songName = string.Join(" ", commandInput).Substring(4);
+
+                    if (!songsQueue.Contains(songName))
+                    {
+                        songsQueue.Enqueue(songName);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{songName} is already contained!");
+                    }
+                }
+                else if (command == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songsQueue));
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
