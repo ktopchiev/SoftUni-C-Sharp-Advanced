685697bfd52f373ff6e147d4a20772ca5cafc32c
ktopchiev <karlostopchiev@gmail.com>
2021-01-15 15:28:24 +0200

    solved basic-stack-operations

---
+++ C-Sharp-Avanced/Stacks-And-Queues/Basic-Stack-Operations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] parameters = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = parameters[0];
+            int s = parameters[1];
+            int x = parameters[2];
+            
+            Stack<int> elements = new Stack<int>();
+
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                elements.Push(nums[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                elements.Pop();
+            }
+            
+            if (elements.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                if (elements.Contains(x))
+                {
+                    Console.WriteLine("true");
+                }
+                else if (!elements.Contains(x))
+                {
+                    Console.WriteLine(elements.Min());
+                }
+            }
+        }
+    }
+}
