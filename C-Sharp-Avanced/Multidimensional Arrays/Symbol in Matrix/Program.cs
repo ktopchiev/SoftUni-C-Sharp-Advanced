5073ed28eeb9c7f73e32dbb6585054dfc892976b
ktopchiev <karlostopchiev@gmail.com>
2021-01-17 18:07:40 +0200

    .

--- C-Sharp-Avanced/Multidimensional Arrays/Symbol in Matrix/Program.cs
+++ C-Sharp-Avanced/Multidimensional Arrays/Symbol in Matrix/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Symbol_in_Matrix
 {
@@ -6,7 +7,45 @@ namespace Symbol_in_Matrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string chars = Console.ReadLine();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = chars[col];
+                }
+            }
+
+            char ch = char.Parse(Console.ReadLine());
+            bool isFound = false;
+            
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row,col] == ch)
+                    {
+                        isFound = true;
+                        Console.WriteLine($"({row}, {col})");
+                        break;
+                    }
+                }
+
+                if (isFound)
+                {
+                    break;
+                }
+            }
+
+            if (!isFound)
+            {
+                Console.WriteLine($"{ch} does not occur in the matrix");
+            }
         }
     }
 }
