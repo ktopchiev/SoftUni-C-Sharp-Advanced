46447e2e446fb192a74066a71472fbabe5dac317
ktopchiev <karlostopchiev@gmail.com>
2021-01-17 20:27:00 +0200

    .

---
+++ C-Sharp-Avanced/Multidimensional Arrays/Diagonal Difference/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Primary_Diagonal
+{
+    class Program
+    {
+        /*
+         * Write a program that finds the difference between the sums of the square matrix diagonals (absolute value).
+         */
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            int sumFirstDiagonal = 0;
+            int sumSecondDiagonal = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputNumbers[col];
+                }
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (col == row)
+                    {
+                        sumFirstDiagonal += matrix[row, col];
+                        sumSecondDiagonal += matrix[row, matrix.GetLength(1) - 1 - col];
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(sumFirstDiagonal-sumSecondDiagonal));
+        }
+    }
+}
