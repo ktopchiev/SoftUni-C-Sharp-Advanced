8ef9077fc3466271da9e8eb151f4200bdf08c5b3
ktopchiev <karlostopchiev@gmail.com>
2021-01-17 15:28:16 +0200

    Sum matrix elements

--- C-Sharp-Avanced/Multidimensional Arrays/Sum Matrix Elements/Program.cs
+++ C-Sharp-Avanced/Multidimensional Arrays/Sum Matrix Elements/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Linq;
 
 namespace Sum_Matrix_Elements
 {
+    /*
+     * Write program that reads a matrix from the console and print:
+         Count of rows
+         Count of columns
+         Sum of all matrix elements
+     */
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] matrixDimensions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = matrixDimensions[0];
+            int cols = matrixDimensions[1];
+
+            int[,] matrix = new int[rows, cols];
+            int sum = 0;
+            
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbersInput = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbersInput[col];
+                    sum += numbersInput[col];
+                }
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
         }
     }
 }
