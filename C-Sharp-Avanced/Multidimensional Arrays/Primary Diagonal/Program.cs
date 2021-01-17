9f46ae40415dacf00d3f031aca26922ecac9aa34
ktopchiev <karlostopchiev@gmail.com>
2021-01-17 17:36:38 +0200

    matrix sum diagonal

--- C-Sharp-Avanced/Multidimensional Arrays/Primary Diagonal/Program.cs
+++ C-Sharp-Avanced/Multidimensional Arrays/Primary Diagonal/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Linq;
 
 namespace Primary_Diagonal
 {
     class Program
     {
+        /*
+         * Write a program that finds the sum of matrix primary diagonal.
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputNumbers[col];
+                    if (col == row)
+                    {
+                        sum += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
