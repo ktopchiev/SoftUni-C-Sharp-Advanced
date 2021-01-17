32405f041875d258d954ce4a41b7157bd44bbf10
ktopchiev <karlostopchiev@gmail.com>
2021-01-17 16:25:59 +0200

    Sum matrix columns

---
+++ C-Sharp-Avanced/Multidimensional Arrays/Sum Matrix Columns/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Matrix_Elements
+{
+    /*
+     * Write program that read a matrix from console and print the sum for each column.
+     * On first line you will get matrix rows.
+     * On the next rows lines, you will get elements for each column separated with a space.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixDimensions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = matrixDimensions[0];
+            int cols = matrixDimensions[1];
+
+            int[,] matrix = new int[rows, cols];
+            int sum = 0;
+            
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbersInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbersInput[col];
+                }
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sum += matrix[row, col];
+                }
+
+                Console.WriteLine(sum);
+                sum = 0;
+            }
+        }
+    }
+}
