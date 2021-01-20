9e9534549708d17534f8b28568371aba903db13f
ktopchiev <karlostopchiev@gmail.com>
2021-01-20 12:14:20 +0200

    .

---
+++ C-Sharp-Avanced/DictionariesAndSets/_03.Periodic_Table/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> chemicalElements = new SortedSet<string>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var element in input)
+                {
+                    chemicalElements.Add(element);    
+                }
+            }
+
+            foreach (var element in chemicalElements)
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
