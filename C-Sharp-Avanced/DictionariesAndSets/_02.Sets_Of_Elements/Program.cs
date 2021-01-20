8e3ecd17f867cb211a2ddc95a9c53097a2cf2d6f
ktopchiev <karlostopchiev@gmail.com>
2021-01-20 12:08:13 +0200

    .

--- C-Sharp-Avanced/DictionariesAndSets/_02.Sets_Of_Elements/Program.cs
+++ C-Sharp-Avanced/DictionariesAndSets/_02.Sets_Of_Elements/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _02.Sets_Of_Elements
 {
@@ -6,7 +7,38 @@ namespace _02.Sets_Of_Elements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] parameters = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int n = int.Parse(parameters[0]);
+            int m = int.Parse(parameters[1]);
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+            
+            for (int i = 0; i < n + m; i++)
+            {
+                if (i >= n)
+                {
+                    int secondSetInput = int.Parse(Console.ReadLine());
+                    secondSet.Add(secondSetInput);
+                }
+                else
+                {
+                    int firstSetInput = int.Parse(Console.ReadLine());
+                    firstSet.Add(firstSetInput);
+                }
+            }
+
+            foreach (var element1 in firstSet)
+            {
+                foreach (var element2 in secondSet)
+                {
+                    if (element1 == element2)
+                    {
+                        Console.Write(element1 + " ");
+                    }
+                }
+            }
         }
     }
 }
