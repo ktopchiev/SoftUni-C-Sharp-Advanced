0daf797e0cee9ab981eb18eb01c0b58bcb6f4fc3
ktopchiev <karlostopchiev@gmail.com>
2021-01-23 17:23:31 +0200

    line numbers solved

---
+++ C-Sharp-Avanced/Streams_Files_and_Directories/FileOperations/Line_numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Line_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader(@"../../../input2.txt");
+
+            using (reader)
+            {
+                string line = reader.ReadLine();
+
+                using (StreamWriter writer = new StreamWriter(@"../../../output2.txt"))
+                {
+                    int lineNum = 1;
+                    
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{lineNum}. {line}");
+                        lineNum++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
