db99e58dda53ea380b1a9cc048516f69ef65cda4
ktopchiev <karlostopchiev@gmail.com>
2021-01-27 22:20:09 +0200

    word count

---
+++ C-Sharp-Avanced/Streams_Files_and_Directories/FileOperations/_04.Copy_Binary_File/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _04.Copy_Binary_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream stream = new FileStream(@"../../../image-analysis.png", FileMode.Open))
+            using (FileStream writeStream = new FileStream(@"../../../file2.png", FileMode.OpenOrCreate))
+            {
+                // create a buffer to hold the bytes 
+                byte[] buffer = new Byte[1024];
+                int bytesRead;
+                int kbs = 0;
+                // while the read method returns bytes
+                // keep writing them to the output stream
+                while ((bytesRead =
+                    stream.Read(buffer, 0, 1024)) > 0)
+                {
+                    writeStream.Write(buffer, 0, bytesRead);
+                }
+            }
+        }
+    }
+}
