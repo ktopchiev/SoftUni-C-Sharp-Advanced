052912a499707b29dedcf0cae55988ce5be6a8cf
ktopchiev <karlostopchiev@gmail.com>
2021-01-27 19:53:04 +0200

    even lines

---
+++ C-Sharp-Avanced/Streams_Files_and_Directories/FileOperations/_01.Even_Lines/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _01.Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader(@"../../../text.txt");
+
+            using (reader)
+            {
+                string line = reader.ReadLine();
+                var lineCounter = 0;
+                char[] chars = {'-', ',', '.', '!', '?'};
+                
+                while (line != null)
+                {
+                    if (lineCounter % 2 == 0)
+                    {
+                        foreach (var chr in chars)
+                        {
+                            if (line.Contains(chr))
+                            {
+                                line = line.Replace(chr, '@');
+                            }
+                        }
+
+                        string[] words = line.Split(" ");
+                        Console.WriteLine(string.Join(" ", words.Reverse()));
+                    }
+
+                    line = reader.ReadLine();
+                    lineCounter++;
+                }
+            }
+        }
+    }
+}
