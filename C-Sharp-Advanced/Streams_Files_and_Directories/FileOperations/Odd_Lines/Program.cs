8de85d8555a4dba621408d0c034a646762c5c6dc
ktopchiev <karlostopchiev@gmail.com>
2021-01-29 16:55:35 +0200

    zip

---
+++ C-Sharp-Advanced/Streams_Files_and_Directories/FileOperations/Odd_Lines/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+namespace Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating new streamreader and put him path for location of input file
+            StreamReader reader = new StreamReader(@"../../../input.txt");
+
+            using (reader)
+            {
+                //Store every read line in string variable
+                string line = reader.ReadLine();
+
+                int counter = 0;
+                
+                //Open new stream writer with path to output file
+                using (var writer = new StreamWriter(@"../../../output.txt"))
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 == 0)
+                        {
+                            //Write in output.txt file
+                            writer.WriteLine(line);
+                        }
+                        
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C-Sharp-Advanced/Streams_Files_and_Directories/FileOperations/_06.Zip_And_Extract/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.IO.Compression;
+ 
+namespace ZipArchive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var zipFile = @"..\..\..\my.zip";
+            var file = "fileForZip.png";
+ 
+            using (var archive = ZipFile.Open(zipFile, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(file, Path.GetFileName(file));
+            }
+        }
+    }
+}
