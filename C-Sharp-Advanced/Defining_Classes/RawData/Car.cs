21ae80b6b3c3fe0b44bc98fbafb8d91f71873ad9
ktopchiev <karlostopchiev@gmail.com>
2021-01-31 13:50:13 +0200

    raw data

---
+++ C-Sharp-Advanced/Defining_Classes/RawData/Car.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace RawData
+{
+    public class Car
+    {
+        public string Model { get; }
+        public Engine Engine { get; }
+        public Cargo Cargo { get; }
+        public List<Tire> Tires { get; }
+        
+        public Car(string model, int engineSpeed, int enginePower, int cargoWeight, string 
+        cargoType, string[] tiresInitialData)
+        {
+            Model = model;
+            Engine = new Engine(engineSpeed,enginePower);
+            Cargo = new Cargo(cargoWeight, cargoType);
+            Tires = new List<Tire>();
+            AddTires(tiresInitialData);
+        }
+
+        private void AddTires(string[] tiresInitialData)
+        {
+            for (int i = 0; i < tiresInitialData.Length; i+= 2)
+            {
+                double pressure = double.Parse(tiresInitialData[i]);
+                int age = int.Parse(tiresInitialData[i + 1]);
+                Tires.Add(new Tire(pressure, age));
+            }
+        }
+    }
+}
---
+++ C-Sharp-Advanced/Defining_Classes/RawData/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RawData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Car> cars = new List<Car>();
+            
+            //Fill list of cars with information about them
+            for (int i = 0; i < n; i++)
+            {
+                string[] carInfoInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string model = carInfoInput[0];
+                int engineSpeed = int.Parse(carInfoInput[1]);
+                int enginePower = int.Parse(carInfoInput[2]);
+                int cargoWeight = int.Parse(carInfoInput[3]);
+                string cargoType = carInfoInput[4];
+                
+                //Get the tires data into array
+                string[] tiresInitialData = new string[8];
+                for (int j = 0; j < tiresInitialData.Length; j++)
+                {
+                    tiresInitialData[j] = carInfoInput[5 + j];
+                }
+                
+                //Initialize new car and add it to the list of cars
+                Car newCar = new Car(model, engineSpeed, enginePower, cargoWeight, cargoType, tiresInitialData);
+                cars.Add(newCar);
+            }
+            
+            //After data processing get the final command results
+            string finalCommand = Console.ReadLine().ToLower();
+
+            switch (finalCommand)
+            {
+                case "fragile":
+                    foreach (var car in cars.Where(x => x.Cargo.Type == "fragile"))
+                    {
+                        bool lowPressure = car.Tires.Any(x => x.Pressure < 1);
+                        
+                        if (lowPressure)
+                        {
+                            Console.WriteLine(car.Model);
+                        }
+                    }
+                    break;
+                case "flamable":
+                    foreach (var car in cars.Where(x => x.Cargo.Type == "flamable"))
+                    {
+                        if (car.Engine.Power > 250)
+                        {
+                            Console.WriteLine(car.Model);
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
