f85cdba223b6ecf98c2cf73a6c8b195382fdaecd
ktopchiev <karlostopchiev@gmail.com>
2021-02-01 15:49:29 +0200

    Pokemon

---
+++ C-Sharp-Advanced/Defining_Classes/Pokemon_trainer/StartUp.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pokemon_trainer
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Trainer> trainers = new List<Trainer>();
+
+            //Getting data
+            while (true)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string trainerName = input[0];
+
+                if (trainerName == "Tournament")
+                {
+                    break;
+                }
+
+                if (input.Length > 3)
+                {
+                    string pokemonName = input[1];
+                    string pokemonElement = input[2];
+                    double pokemonHealth = 0;
+                    
+                    bool isDouble = double.TryParse(input[3], out pokemonHealth);
+                    
+                    Pokemon newPokemon;
+
+                    if (trainers.All(x => x.Name != trainerName))
+                    {
+                        Trainer newTrainer = new Trainer(trainerName);
+                        newPokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+                        newTrainer.Pokemons.Add(newPokemon);
+                        trainers.Add(newTrainer);
+                    }
+                    else
+                    {
+                        newPokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+                        var trainer = trainers.Find(x => x.Name == trainerName);
+                        trainer.Pokemons.Add(newPokemon);
+                    }
+                }
+            }
+            
+            //Tournament mode
+            while (true)
+            {
+                string elementInput = Console.ReadLine();
+
+                if (elementInput == "End")
+                {
+                    break;
+                }
+                
+                foreach (var trainer in trainers)
+                {
+                    bool hasGivenElement = trainer.Pokemons.Any(x => x.Element == elementInput);
+
+                    if (hasGivenElement)
+                    {
+                        trainer.AddToBadge();
+                    }
+                    else if (trainers.Count > 1)
+                    {
+                        trainer.AttackPokemons();
+                    }
+
+                    if (trainer.Pokemons.Count > 0)
+                    {
+                        foreach (var pokemon in trainer.Pokemons)
+                        {
+                            if (pokemon.Health <= 0)
+                            {
+                                trainer.Pokemons.Remove(pokemon);
+                                if (trainer.Pokemons.Count == 0)
+                                {
+                                    break;
+                                }
+                            }
+                        }   
+                    }
+                }
+            }
+
+            if (trainers.Count > 0)
+            {
+                foreach (var trainer in trainers.OrderByDescending(x => x.GetBadges()))
+                {
+                    Console.Write($"{trainer.Name} {trainer.GetBadges()} {trainer.Pokemons.Count}");
+                    Console.WriteLine();
+                }   
+            }
+        }
+    }
+}
---
+++ C-Sharp-Advanced/Defining_Classes/Pokemon_trainer/Trainer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Pokemon_trainer
+{
+    public class Trainer
+    {
+        public string Name { get; set; }
+        private int badges;
+        public List<Pokemon> Pokemons { get; set; }
+
+        public Trainer(string name)
+        {
+            Name = name;
+            badges = 0;
+            Pokemons = new List<Pokemon>();
+        }
+
+        public void AddToBadge()
+        {
+            badges += 1;
+        }
+
+        public int GetBadges()
+        {
+            return badges;
+        }
+
+        public void AttackPokemons()
+        {
+            foreach (var pokemon in Pokemons)
+            {
+                pokemon.Health -= 10;
+            }
+        }
+    }
+}
