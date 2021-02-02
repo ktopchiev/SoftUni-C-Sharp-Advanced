6685d7f63a3ec92fa3d91d269130a6970386dfab
ktopchiev <karlostopchiev@gmail.com>
2021-02-02 14:33:04 +0200

    action print function

---
+++ C-Sharp-Advanced/Functional_Programming/Action_Print/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Action_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).ToList();
+            Action<List<string>> print = message => Console.WriteLine(string.Join("\n",message));
+            print(names);
+        }
+    }
+}
---
+++ C-Sharp-Advanced/Functional_Programming/Knights_of_Horror/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Knights_of_Horror
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
