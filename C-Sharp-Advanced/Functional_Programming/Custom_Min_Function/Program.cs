7cb278252cb49592f89ceccfabd92e3e340acd72
ktopchiev <karlostopchiev@gmail.com>
2021-02-02 15:43:57 +0200

    custom min integer

---
+++ C-Sharp-Advanced/Functional_Programming/Custom_Min_Function/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Custom Min function
+            HashSet<int> setOfIntegers = Console.ReadLine().Split(" ", StringSplitOptions
+                .RemoveEmptyEntries).Select(int.Parse).ToHashSet();
+
+            Func<HashSet<int>, int> Min = GetMin;
+            Console.WriteLine(Min(setOfIntegers));
+        }
+
+        static int GetMin(HashSet<int> integers)
+        {
+            int min = integers.First();
+            
+            foreach (var integer in integers)
+            {
+                if (integer < min)
+                {
+                    min = integer;
+                }
+            }
+
+            return min;
+        }
+    }
+}
