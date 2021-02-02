b308034e5a08c6ea094597fcd892709fa1213491
ktopchiev <karlostopchiev@gmail.com>
2021-02-02 22:02:17 +0200

    predicate for names

---
+++ C-Sharp-Advanced/Functional_Programming/Predicate_for_names/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Predicate_for_names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Predicate<string> findNames = names => names.Length <= n;
+            List<string> names = new List<string>();
+            
+            Console.WriteLine(string.Join("\n",
+                 names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .FindAll(findNames)));
+        }
+    }
+}
