484ca80217cbc71fd42a91416f68a04ad7d59556
ktopchiev <karlostopchiev@gmail.com>
2021-02-02 17:17:06 +0200

    delegates

---
+++ C-Sharp-Advanced/Functional_Programming/Reverse_and_Exclude/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reverse_and_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get a collection of numbers
+            int[] collectionOfNums = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            Func<int[], List<int>> reverse = ReverseCollection;
+            
+            // Reverse the numbers from the collection using Func<T,T>
+            List<int> reversedList = reverse(collectionOfNums);
+
+            Predicate<int> divisibleNum = num => num % n == 0;
+            
+            //Remove all numbers that are divisible by n
+            reversedList.RemoveAll(divisibleNum);
+
+            Console.WriteLine(string.Join(" ", reversedList));
+
+        }
+
+        static List<int> ReverseCollection(int[] collection)
+        {
+            List<int> reversedList = new List<int>();
+
+            for (int i = 0; i < collection.Length / 2; i++)
+            {
+                int tempNum = collection[i];
+                collection[i] = collection[collection.Length - 1 - i];
+                collection[collection.Length - 1 - i] = tempNum;
+            }
+
+            reversedList = collection.ToList();
+            return reversedList;
+        }
+    }
+}
