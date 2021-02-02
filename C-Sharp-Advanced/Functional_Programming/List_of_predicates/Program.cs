38b54672661c28b7f882f8dd7c09af12b8af410a
ktopchiev <karlostopchiev@gmail.com>
2021-02-02 22:46:17 +0200

    predicate list

---
+++ C-Sharp-Advanced/Functional_Programming/List_of_predicates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace List_of_predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
