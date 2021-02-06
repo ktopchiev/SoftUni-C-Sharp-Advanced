b15d5f14b78f64cfc2bf01cdfe1756191acec258
ktopchiev <karlostopchiev@gmail.com>
2021-02-06 22:04:09 +0200

    box of strings

---
+++ C-Sharp-Advanced/Generics/GenericSwapMethodStrings/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] myList = new int[n];
+            
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                myList[i] = int.Parse(input);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Swap(myList, indexes[0], indexes[1]);
+
+            foreach (var item in myList)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+        }
+        
+        private static T[] Swap<T>(T[] list, int x, int y)
+        {
+            T[] myList = list;
+
+            T temp = list[x];
+            list[x] = list[y];
+            list[y] = temp;
+
+            return myList;
+        }
+    }
+}
