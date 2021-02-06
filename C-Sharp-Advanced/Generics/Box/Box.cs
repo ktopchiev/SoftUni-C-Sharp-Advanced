1e166f7f152611b15eb56155d27094be029e95b1
ktopchiev <karlostopchiev@gmail.com>
2021-02-06 21:30:50 +0200

    box of strings

---
+++ C-Sharp-Advanced/Generics/Box/Box.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Box
+{
+    public class Box<T>
+    {
+        private T box;
+
+        public Box(T value)
+        {
+            this.box = value;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{box.GetType()}: ");
+            sb.Append($"{box}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ C-Sharp-Advanced/Generics/Box/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string str = Console.ReadLine();
+                Box<string> newBox = new Box<string>(str);
+                Console.WriteLine(newBox.ToString());
+            }
+        }
+    }
+}
