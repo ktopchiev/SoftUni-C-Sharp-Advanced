798ad175655a2c013f417c3dd66b1ffd37f4149d
ktopchiev <karlostopchiev@gmail.com>
2021-02-06 20:28:51 +0200

    /

---
+++ C-Sharp-Advanced/Generics/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] pesho = ArrayCreator.Create(5, "Pesho");
+            int[] fours = ArrayCreator.Create(10, 4);
+            Stack<int>[] stacks = ArrayCreator.Create(5, new Stack<int>());
+        }
+    }
+}
