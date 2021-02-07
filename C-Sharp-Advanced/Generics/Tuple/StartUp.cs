e857282343ecfb18e539c3714286df4d2f04cfd7
ktopchiev <karlostopchiev@gmail.com>
2021-02-07 18:50:37 +0200

    .

---
+++ C-Sharp-Advanced/Generics/Tuple/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            Tuple<string, string, string> tuple1 = new Tuple<string, string, string>(input[0] + " " + input[1], input[2], input[3]);
+            if (input.Length > 4)
+            {
+                tuple1 = new Tuple<string, string, string>(input[0] + " " + input[1], input[2],
+                    input[3] + " " + input[4]);
+            }
+            Console.WriteLine(tuple1);
+
+            input = Console.ReadLine().Split();
+            Tuple<string, int, string> tuple2 = new Tuple<string, int, string>(input[0], int.Parse
+            (input[1]), input[2] == "drunk" ? "True" : "False");
+            Console.WriteLine(tuple2);
+
+            input = Console.ReadLine().Split();
+            Tuple<string, double, string> tuple3 = new Tuple<string, double, string>(input[0],
+            double.Parse(input[1]), input[2]);
+            if (input.Length > 3)
+            {
+                tuple3 = new Tuple<string, double, string>(input[0],
+                    double.Parse(input[1]), input[2] + " " + input[3]);
+            }
+            Console.WriteLine(tuple3);
+        }
+    }
+}
---
+++ C-Sharp-Advanced/Generics/Tuple/Tuple.cs
@@ -0,0 +1,24 @@
+﻿namespace Tuple
+{
+    public class Tuple<T1,T2,T3>
+    {
+        public object Key { get; set; }
+        public object Value { get; set; }
+        
+        public object Value2 { get; set; }
+
+        public Tuple(T1 key, T2 value, T3 value2)
+        {
+            Key = key;
+            Value = value;
+            Value2 = value2;
+        }
+        
+        
+
+        public override string ToString()
+        {
+            return $"{Key} -> {Value} -> {Value2}";
+        }
+    }
+}
