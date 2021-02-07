c183c9ca9a4f9d21fdcf8b9c0be5429bde426ea1
ktopchiev <karlostopchiev@gmail.com>
2021-02-07 15:09:37 +0200

    .

---
+++ C-Sharp-Advanced/Generics/GenericCountMethodStrings/Box.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCountMethodStrings
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        private List<T> box;
+
+        public Box(List<T> newList)
+        {
+            box = newList;
+        }
+
+        public int CountString(T element)
+        {
+            int counter = 0;
+            T elementToCompare = element;
+            
+            foreach (var item in box)
+            {
+                if (item.CompareTo(elementToCompare) == 1)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+        
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{box.GetType()}: ");
+            sb.Append($"{box}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ C-Sharp-Advanced/Generics/GenericCountMethodStrings/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            List<string> myList = new List<string>();
+            
+            for (int i = 1; i <= n; i++)
+            {
+                myList.Add(Console.ReadLine().Trim());
+            }
+
+            Box<string> myBox = new Box<string>(myList);
+            string itemForSearch = Console.ReadLine().Trim();
+
+            Console.WriteLine(myBox.CountString(itemForSearch));
+        }
+    }
+}
