a2f3828de8ceae135f175a84583b90e32799ebc0
ktopchiev <karlostopchiev@gmail.com>
2021-02-10 23:08:48 +0200

    bakery

---
+++ C-Sharp-Advanced/CSharp-Advanced-Retake-Exam-16-December-2020/_03.Openning/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+
+        public Employee(string name, int age, string country)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"Employee: {Name}, ");
+            sb.Append($"{Age}");
+            sb.Append($"({Country})");
+
+            return sb.ToString();
+        }
+    }
+}
