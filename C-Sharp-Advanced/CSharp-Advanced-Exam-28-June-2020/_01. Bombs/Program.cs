df144ec9c203741fb2b324ecdd4da0ae1b6bf576
ktopchiev <karlostopchiev@gmail.com>
2021-02-11 20:03:50 +0200

    .

---
+++ C-Sharp-Advanced/CSharp-Advanced-Exam-28-June-2020/_01. Bombs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
