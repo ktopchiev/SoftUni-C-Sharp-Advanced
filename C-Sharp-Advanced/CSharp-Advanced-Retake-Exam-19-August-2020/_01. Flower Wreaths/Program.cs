617438f064b9525f3eb1b84cd6b013d8328124d5
ktopchiev <karlostopchiev@gmail.com>
2021-02-13 11:37:44 +0200

    .

---
+++ C-Sharp-Advanced/CSharp-Advanced-Retake-Exam-19-August-2020/_01. Flower Wreaths/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Flower_Wreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
