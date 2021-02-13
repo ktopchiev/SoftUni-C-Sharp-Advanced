0221b3b1a2c9be53cadd4fe5c3cfba6203e3f011
ktopchiev <karlostopchiev@gmail.com>
2021-02-13 14:47:04 +0200

    removed zip

--- C-Sharp-Advanced/CSharp-Advanced-Exam-25-October-2020/_01. Scheduling/Program.cs
+++ C-Sharp-Advanced/CSharp-Advanced-Exam-25-October-2020/_01. Scheduling/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
 
 namespace _01._Scheduling
 {
@@ -6,7 +8,41 @@ namespace _01._Scheduling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Getting the input
+            int[] tasks = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] threads = Console.ReadLine().Split(" ", StringSplitOptions
+                .RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int taskToBeKilled = int.Parse(Console.ReadLine());
+            
+            Queue<int> threadsContainer = new Queue<int>(threads);
+            Stack<int> tasksContainer = new Stack<int>(tasks);
+
+            while (threadsContainer.Count > 0 && tasksContainer.Count > 0)
+            {
+                
+                if (tasksContainer.Peek() == taskToBeKilled)
+                {
+                    Console.WriteLine($"Thread with value {threadsContainer.Peek()} killed task {taskToBeKilled}");
+                    break;
+                }
+                
+                if (threadsContainer.Peek() >= tasksContainer.Peek())
+                {
+                    threadsContainer.Dequeue();
+                    tasksContainer.Pop();
+                }
+                else if (threadsContainer.Peek() < tasksContainer.Peek())
+                {
+                    threadsContainer.Dequeue();
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", threadsContainer));
         }
     }
 }
