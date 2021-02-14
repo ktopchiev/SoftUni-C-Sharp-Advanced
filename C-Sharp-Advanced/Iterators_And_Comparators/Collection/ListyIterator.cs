ccee252a3ae5bfc2703283072cf3f5d25ae6afde
ktopchiev <karlostopchiev@gmail.com>
2021-02-14 18:00:31 +0200

    iterators

---
+++ C-Sharp-Advanced/Iterators_And_Comparators/Collection/ListyIterator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collection
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private readonly List<T> _data;
+        private int _internalIndexPosition = 0;
+        
+        public ListyIterator(params T[] input)
+        {
+            _data = new List<T>(input);
+        }
+
+        public bool Move()
+        {
+            _internalIndexPosition++;
+            if (_internalIndexPosition < _data.Count)
+            {
+                return true;
+            }
+            
+            _internalIndexPosition--;
+            return false;
+            
+        }
+
+        public bool HasNext()
+        {
+            return _internalIndexPosition + 1 < _data.Count;
+        }
+
+        public void Print()
+        {
+            if (_data.Count > 0)
+                Console.WriteLine(_data[_internalIndexPosition]);
+            else
+                Console.WriteLine("Invalid Operation");
+        }
+
+        public void PrintAll()
+        {
+            foreach (var element in _data)
+            {
+                Console.Write(element + " ");
+            }
+
+            Console.WriteLine();
+        }
+        
+        public ListyIterator(IEnumerable<T> data)
+        {
+            _data = data.ToList();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _data.Count; i++)
+            {
+                yield return _data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C-Sharp-Advanced/Iterators_And_Comparators/Collection/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collection
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> myListyIterator = new ListyIterator<string>();
+            
+            List<string> command = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            if (command.Count == 0)
+            {
+                myListyIterator = new ListyIterator<string>();
+            }
+                
+            if (command[0] == "Create")
+            {
+                command.RemoveAt(0);
+                myListyIterator = new ListyIterator<string>(command);
+            }
+
+            while (true)
+            {
+                command = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                if (command[0] == "END")
+                {
+                    break;
+                }
+                
+                if (command[0] == "Move")
+                {
+                    Console.WriteLine(myListyIterator.Move());
+                }
+                else if (command[0] == "Print")
+                {
+                    myListyIterator.Print();
+                }
+                else if (command[0] == "HasNext")
+                {
+                    Console.WriteLine(myListyIterator.HasNext());
+                }
+                else if (command[0] == "PrintAll")
+                {
+                    myListyIterator.PrintAll();
+                }
+            }
+        }
+    }
+}
