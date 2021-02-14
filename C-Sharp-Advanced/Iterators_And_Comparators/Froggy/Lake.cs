2aa9c50834688061b5f0acd4a63ec62c1e3baec6
ktopchiev <karlostopchiev@gmail.com>
2021-02-14 19:31:47 +0200

    custom stack

---
+++ C-Sharp-Advanced/Iterators_And_Comparators/Froggy/Lake.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Froggy
+{
+    public class Lake<T> : IEnumerable<T>
+    {
+        private readonly List<T> _stones;
+
+        public Lake()
+        {
+            _stones = new List<T>();
+        }
+
+        public Lake(params T[] stones)
+        {
+            _stones = new List<T>();
+            
+            for (int i = 0; i < stones.Length; i += 2)
+            {
+                _stones.Add(stones[i]);
+            }
+
+            int index = _stones.Count;
+            
+            for (int i = 1; i < stones.Length ; i +=2)
+            {
+                _stones.Insert(index, stones[i]);
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(string.Join(", ", _stones));
+        }
+        
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _stones.Count; i++)
+            {
+                yield return _stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+}
---
+++ C-Sharp-Advanced/Iterators_And_Comparators/Froggy/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] stonesInput = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            
+            Lake<int> newLake = new Lake<int>(stonesInput);
+            
+            newLake.Print();
+        }
+    }
+}
