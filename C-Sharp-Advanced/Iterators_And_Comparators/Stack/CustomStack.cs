e536d4c9dec62a1958206342f09c22fa0852f5bb
ktopchiev <karlostopchiev@gmail.com>
2021-02-14 18:44:37 +0200

    custom stack

---
+++ C-Sharp-Advanced/Iterators_And_Comparators/Stack/CustomStack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private readonly List<T> _stack;
+
+        public CustomStack()
+        {
+            _stack = new List<T>();
+        }
+        
+        public void Push(params T[] elements)
+        {
+            foreach (var element in elements)
+            {
+                _stack.Add(element);
+            }
+        }
+
+        public object Pop()
+        {
+            object lastElement = null;
+            
+            if(_stack.Count > 0)
+            { 
+                lastElement = _stack.Last();
+                _stack.RemoveAt(_stack.Count - 1);
+            }
+            else
+            {
+                Console.WriteLine("No elements");
+            }
+            
+            return lastElement;
+        }
+        
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = _stack.Count - 1; i >= 0; i--)
+            {
+                yield return _stack[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C-Sharp-Advanced/Iterators_And_Comparators/Stack/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Stack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<int> myCustomStack = new CustomStack<int>();
+
+            while (true)
+            {
+               string[] commands = Console.ReadLine().Split(new []{" ",", " }, StringSplitOptions.RemoveEmptyEntries);
+
+               if (commands[0] == "END")
+               {
+                   break;
+               }
+                
+               switch (commands[0])
+                {
+                    case "Push":
+                    {
+                        for (int i = 1; i < commands.Length; i++)
+                        {
+                            myCustomStack.Push(int.Parse(commands[i]));
+                        }
+
+                        break;
+                    }
+                    case "Pop":
+                        myCustomStack.Pop();
+                        break;
+                }
+            }
+
+            foreach (var element in myCustomStack)
+            {
+                Console.WriteLine(element);
+            }
+            
+            foreach (var element in myCustomStack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
