0471a8275a8dbbe928659b90f5fd764c5d4b9839
ktopchiev <karlostopchiev@gmail.com>
2021-02-17 11:09:56 +0200

    .

---
+++ C-Sharp-Advanced/Iterators_And_Comparators/Comparing Objects/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+        }
+
+        private string Name { get; }
+
+        private int Age { get; }
+
+        private string Town { get; }
+
+        public int CompareTo(Person other)
+        {
+            if (Name != other.Name)
+            {
+                return Name.CompareTo(other.Name);
+            }
+
+            if (Age != other.Age)
+            {
+                return Age.CompareTo(other.Age);
+            }
+
+            return Town != other.Town ? Town.CompareTo(other.Town) : 0;
+        }
+    }
+}
