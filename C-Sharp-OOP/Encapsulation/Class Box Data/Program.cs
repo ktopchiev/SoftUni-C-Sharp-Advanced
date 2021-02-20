62933e99fd8db50eccd9bf9c626b78d967b25906
ktopchiev <karlostopchiev@gmail.com>
2021-02-20 19:13:12 +0200

    Initial Commit

---
+++ C-Sharp-OOP/Encapsulation/Class Box Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class_Box_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
