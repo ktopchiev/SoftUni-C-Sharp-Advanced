54cd29294f099af680d829e6fd1162a473aad297
ktopchiev <karlostopchiev@gmail.com>
2021-02-24 19:16:35 +0200

    person

---
+++ C-Sharp-OOP/Inheritance/Person/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append(String.Format("Name: {0}, Age: {1}", Name, Age));
+
+            return str.ToString();
+        }
+    }
+}
