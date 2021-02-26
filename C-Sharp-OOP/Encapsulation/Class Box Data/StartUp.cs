acdbfa111956323b9150473921f1056fd98cbae0
ktopchiev <karlostopchiev@gmail.com>
2021-02-26 21:54:47 +0200

    encapsulation

---
+++ C-Sharp-OOP/Encapsulation/Class Box Data/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double length = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+
+                Box box = new Box(length, width, height);
+
+                Console.WriteLine($"Surface Area - {box.GetSurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.GetLateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.GetVolume():f2}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
