16be4aa35f6829ea83c63201089d4aea7a0fa0c6
ktopchiev <karlostopchiev@gmail.com>
2021-03-03 16:25:34 +0200

    cars

---
+++ C-Sharp-OOP/Interfaces_And_Abstraction/Cars/ICar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    interface ICar
+    {
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public void Start();
+
+        public void Stop();
+    }
+}
