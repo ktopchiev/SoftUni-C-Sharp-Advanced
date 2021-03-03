1d9c6b23e4d2a4c672ea4f95aa7de56e6ad39320
ktopchiev <karlostopchiev@gmail.com>
2021-03-03 16:46:02 +0200

    changed person info

--- C-Sharp-OOP/Interfaces_And_Abstraction/Define_and_Interface_IPerson/Citizen.cs
+++ C-Sharp-OOP/Interfaces_And_Abstraction/Define_and_Interface_IPerson/Citizen.cs
@@ -4,18 +4,24 @@ using System.Text;
 
 namespace PersonInfo
 {
-    public class Citizen : IPerson
+    public class Citizen : IPerson, IIdentifiable, IBirthable
     {
         private string name;
         private int age;
+        private string id;
+        private string birthDate;
 
-        public Citizen(string name, int age)
+        public Citizen(string name, int age, string id, string birthDate)
         {
             Name = name;
             Age = age;
+            Id = id;
+            Birthdate = birthDate;
         }
 
         public string Name { get => name; set => name = value; }
         public int Age { get => age; set => age = value; }
+        public string Id { get => id; set => id = value; }
+        public string Birthdate { get => birthDate; set => birthDate = value; }
     }
 }
