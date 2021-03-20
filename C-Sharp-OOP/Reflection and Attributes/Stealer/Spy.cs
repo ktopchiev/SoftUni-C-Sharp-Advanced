9b36aa9253a9bcf7a7dd43f0c47f606fc7a02a39
ktopchiev <karlostopchiev@gmail.com>
2021-03-20 22:13:50 +0200

    .

--- C-Sharp-OOP/Reflection and Attributes/Stealer/Spy.cs
+++ C-Sharp-OOP/Reflection and Attributes/Stealer/Spy.cs
@@ -9,13 +9,19 @@ namespace Stealer
     {
         public string StealFieldInfo(string investigatedClass, params string[] investigatedFields)
         {
+            //Get type of a class from passed string variable
             Type searchedClass = Type.GetType(investigatedClass);
+
+            //Create new FieldInfo array to hold the fields
             FieldInfo[] fields = new FieldInfo[investigatedFields.Length];
 
+            //Create new instance of the investigated class
             Hacker hacker = new Hacker();
 
+            //Create stringbuilder
             StringBuilder sb = new StringBuilder($"Class under investigation: {searchedClass.FullName}{Environment.NewLine}");
 
+            //Get the fields from the investigated class
             for (int i = 0; i < investigatedFields.Length; i++)
             {
                 fields[i] = searchedClass.GetField(investigatedFields[i],
@@ -25,6 +31,7 @@ namespace Stealer
                     BindingFlags.Instance);
             }
 
+            //Get the values of fields of the investigated class from his instance
             foreach (var field in fields)
             {
                 sb.AppendLine($"{field.Name}: {field.GetValue(hacker)}");
