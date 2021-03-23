36fdcae9913ed8c9c753d7914d6fe573ff1e5d68
ktopchiev <karlostopchiev@gmail.com>
2021-03-23 15:41:09 +0200

    Unit testing

---
+++ C-Sharp-OOP/Unit Testing/Test Axe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_Axe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
