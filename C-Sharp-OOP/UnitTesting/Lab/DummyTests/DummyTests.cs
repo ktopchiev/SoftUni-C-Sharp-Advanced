cad66dbec42d3652919acc84ca7a29598f731ffe
ktopchiev <karlostopchiev@gmail.com>
2021-03-25 22:21:02 +0200

    .

---
+++ C-Sharp-OOP/UnitTesting/Lab/DummyTests/DummyTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace DummyTests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+
+        [Test]
+        public void When_DummyIsAttacked_Should_LoseHealth()
+        {
+            Dummy dummy = new Dummy(100, 100);
+
+            dummy.TakeAttack(50);
+
+            Assert.That(dummy.Health, Is.LessThan(100), "Dummy does not decrease health points.");
+        }
+
+        [Test]
+        public void When_DummyIsDead_Should_ThrowExceptionIfAttacked()
+        {
+            Dummy dummy = new Dummy(0, 100);
+
+            Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(1));
+        }
+
+        [Test]
+        public void When_DummyIsDead_Should_CanGiveExperience()
+        {
+            Dummy dummy = new Dummy(0, 100);
+
+            Assert.That(dummy.GiveExperience, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void When_DummyIsAlive_Should_CannotGiveExperience()
+        {
+            Dummy dummy = new Dummy(100, 100);
+
+            Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+        }
+    }
+}
---
+++ C-Sharp-OOP/UnitTesting/Lab/Tests/AxeTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [Test]
+        public void When_AxeTakesAttack_Should_DecreaseItsDurability()
+        {
+            Axe axe = new Axe(10,10);
+
+            axe.Attack(new Dummy(5,5));
+
+            Assert.That(axe.DurabilityPoints, Is.LessThan(10), "Axe does not decrease its durability.");
+        }
+    }
+}
