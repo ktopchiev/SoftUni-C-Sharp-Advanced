685a1bec31b9a0f1e0a366552d3cff240455d7b6
ktopchiev <karlostopchiev@gmail.com>
2021-03-27 23:34:38 +0200

    .

--- C-Sharp-OOP/UnitTesting/FightingArena.Tests/ArenaTests.cs
+++ C-Sharp-OOP/UnitTesting/FightingArena.Tests/ArenaTests.cs
@@ -1,18 +1,90 @@
-﻿using NUnit.Framework;
+﻿//using FightingArena;
+using NUnit.Framework;
+using System;
+using System.Linq;
 
 namespace Tests
 {
     public class ArenaTests
     {
+        private Arena arena;
+        private Warrior warrior;
+        private string name;
+        private int damage;
+        private int hp;
+
         [SetUp]
         public void Setup()
         {
+            arena = new Arena();
+            name = "ShadowBlade";
+            damage = 60;
+            hp = 100;
+            warrior = new Warrior(name, damage, hp);
         }
 
         [Test]
-        public void Test1()
+        public void Ctor_Arena_ShouldBeSetProperly()
         {
-            Assert.Pass();
+            Assert.That(arena.Warriors.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Warriors_CanBeEnrolledAnd_Should_HaveCount()
+        {
+            arena.Enroll(warrior);
+
+            Assert.That(arena.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Enroll_Should_ThrowException_WhenWarriorExists()
+        {
+            arena.Enroll(warrior);
+            Assert.Throws<InvalidOperationException>(() => arena.Enroll(warrior));
+        }
+
+        [Test]
+        public void Fight_Should_FindTwoWarriorsByName_OneAttacker_And_OneDeffender()
+        {
+            arena.Enroll(new Warrior("Attacker", 80, 100));
+            arena.Enroll(new Warrior("Deffender", 90, 100));
+
+            arena.Fight("Attacker", "Deffender");
+
+            var attacker = arena.Warriors.FirstOrDefault(x => x.Name == "Attacker");
+            var deffender = arena.Warriors.FirstOrDefault(x => x.Name == "Deffender");
+
+            Assert.IsTrue(arena.Warriors.Contains(attacker));
+            Assert.IsTrue(arena.Warriors.Contains(deffender));
+        }
+
+        [Test]
+        [TestCase(null)]
+        public void Fight_Should_ThrowException_WhenAttackerArgumentIsNull(string attackerName)
+        {
+            arena.Enroll(warrior);
+            Assert.Throws<InvalidOperationException>(() => arena.Fight(attackerName, warrior.Name));
+        }
+
+        [Test]
+        [TestCase(null)]
+        public void Fight_Should_ThrowException_WhenDeffenderArgumentIsNull(string deffenderName)
+        {
+            arena.Enroll(warrior);
+            Assert.Throws<InvalidOperationException>(() => arena.Fight(warrior.Name, deffenderName));
+        }
+
+        [Test]
+        public void Fight_Should_PerformAttackAndDecreaseWarriorHealth()
+        {
+            arena.Enroll(new Warrior("Blade", 50, 100));
+
+            arena.Enroll(warrior);
+
+            arena.Fight("Blade", warrior.Name);
+
+            Assert.That(warrior.HP, Is.EqualTo(50));
         }
     }
 }
--- C-Sharp-OOP/UnitTesting/FightingArena.Tests/WarriorTests.cs
+++ C-Sharp-OOP/UnitTesting/FightingArena.Tests/WarriorTests.cs
@@ -1,18 +1,98 @@
 using NUnit.Framework;
+//using FightingArena;
+using System;
+
 
 namespace Tests
 {
     public class WarriorTests
     {
+        private Warrior warrior;
+
         [SetUp]
         public void Setup()
         {
+            warrior = new Warrior("Axe", 75, 100);
         }
 
         [Test]
-        public void Test1()
+        public void Ctor_Warrior_SetProperly()
         {
-            Assert.Pass();
+            Assert.That(warrior.Name, Is.EqualTo("Axe"));
+            Assert.That(warrior.Damage, Is.EqualTo(75));
+            Assert.That(warrior.HP, Is.EqualTo(100));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Name_Property_Should_ThrowException_WhenArgumentIsNullOrEmptyOrWhitespace(string name)
+        {
+            Assert.Throws<ArgumentException>(() => warrior = new Warrior(name, 75, 100));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void Damage_Property_ShouldThrowException_WhenArgumentIsZeroOrNegative(int damage)
+        {
+            Assert.Throws<ArgumentException>(() => warrior = new Warrior("Pesho", damage, 100));
+        }
+
+        [Test]
+        public void HP_Property_Should_ThrowException_WhenArgumentIsNegative()
+        {
+            Assert.Throws<ArgumentException>(() => warrior = new Warrior("Pesho", 20, -5));
+        }
+
+        [Test]
+        [TestCase(20)]
+        [TestCase(30)]
+        public void Attack_Should_ThrowException_WhenAttackerHPIsBelowOrEqualToMinAttackHP(int hp)
+        {
+            warrior = new Warrior("ShadowBlade", 60, hp);
+
+            Assert.Throws<InvalidOperationException>(() => warrior.Attack(new Warrior("LightBlade", 20, 40)));
+        }
+
+        [Test]
+        [TestCase(20)]
+        [TestCase(30)]
+        public void Attack_Should_ThrowException_WhenDeffenderHPIsBelowOrEqualToMinAttackHP(int hp)
+        {
+            warrior = new Warrior("ShadowBlade", 60, 50);
+
+            Assert.Throws<InvalidOperationException>(() => warrior.Attack(new Warrior("LightBlade", 20, hp)));
+        }
+
+        [Test]
+        public void Attack_Should_ThrowException_WhenAttackerHPIsLowerThanDeffenderDamage()
+        {
+            Assert.Throws<InvalidOperationException>(() => warrior.Attack(new Warrior("LightBlade", 120, 100)));
+        }
+
+        [Test]
+        public void Attack_Should_DecreaseAttackerHPByDeffenderDamage()
+        {
+            warrior.Attack(new Warrior("LightBlade", 20, 100));
+            Assert.That(warrior.HP, Is.EqualTo(80));
+        }
+
+        [Test]
+        public void Attack_Should_SetToZeroDeffenderDamage_WhenAttackerDamageIsBiggerThanDeffenderHP()
+        {
+            var deffender = new Warrior("LightBlade", 20, 70);
+            warrior.Attack(deffender);
+            Assert.That(deffender.HP, Is.Zero);
+        }
+
+        [Test]
+        public void Attack_Should_DecreaseDeffenderHPByAttackerDamageValue()
+        {
+            var deffender = new Warrior("LightBlade", 20, 100);
+            warrior.Attack(deffender);
+            Assert.That(deffender.HP, Is.EqualTo(25));
         }
     }
 }
